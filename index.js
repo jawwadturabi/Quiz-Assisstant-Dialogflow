3a8236c8586abb6e2984351cc8967e1572501ae8
jawwad <memonjawwad9@gmail.com>
2019-04-26 16:15:21 +0500

    wmultiple quizes

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ var userDetail = new mongoose.Schema(
         Roll_No: { type: String, required: true },
         Total_Score_in_History: { type: String, required: true },
     },
-    { collection: "userData" }
+    // { collection: "userData" }
 );
 var model =  new mongoose.model("userData", userDetail);
 //making schema for collection already exist in db
@@ -42,7 +42,7 @@ var userDetail1 = new mongoose.Schema(
     {
         Question: { type: [String], required: true },
     },
-    { collection: "Questions" }
+    // { collection: "Questions" }
 );
 var model1 = new mongoose.model("Questions", userDetail1);
 module.exports ={
