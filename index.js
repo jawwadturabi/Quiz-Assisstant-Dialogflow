1ba04776b49cc15cf5640aa25097d3cbafadf809
jawwad <memonjawwad9@gmail.com>
2019-04-28 23:58:57 +0500

    Features Added

--- index.js
+++ index.js
@@ -64,7 +64,7 @@ app.post("/webhook", function (request, response, next) {
                     agent.add(new Suggestion(`Science`));
                     agent.add(new Suggestion(`History`));
                 }
-                else {
+                else if (data[0].Total_Score_in_GK) {
                     var gkCh = data[0].Total_Score_in_GK
                     var sciCh = data[0].Total_Score_in_Science
                     var hisCh = data[0].Total_Score_in_History
