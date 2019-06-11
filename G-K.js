0eae9d5f5ec093776e1c69357c94b3df62170236
jawwad <memonjawwad9@gmail.com>
2019-06-11 19:15:25 +0500

    Features Added

--- G-K.js
+++ G-K.js
@@ -1,35 +1,31 @@
-var postmark = require("postmark");
 process.env.DEBUG = "dialogflow:debug"
 const Model = require("./schema.js").model
 const Model1 = require("./schema.js").model1
 
 exports.gk = async (agent) => {
-    const usrCmd = agent.parameters['usrcmd'];
+
     var ourContext = agent.getContext("abc")
-    var score = 0; var increment = 1; var i = 0;
+    var score = 0;
+    var increment = 1;
+    var i = 0;
     var i1; var i2; var i3; var i4; var i5; var i6; //differnt variables to store in context
     var i7; var i8; var i9;
     var score1; var score2; var score3; var score4; var score5;
     var score6; var score7; var score8; var score9; var score10;
     const opt = agent.parameters['option'];
-    if (!opt && !usrCmd) {
+    const ask = agent.parameters['ask'];
+    if (!opt && !ask) {
+
         console.log("ques 1 triggered")
         console.log("context are : ", ourContext)
         await Model1.find({}).then(data => {
             agent.add(`${data[0].Question[i]}`)
-            agent.setContext({
-                name: "abc",
-                lifespan: 5,
-                "parameters": {
-                    "Q": data[0].Question[i]
-                }
-            });
         }).catch(err => {
             console.log("Error is : ", err)
         })
     }
 
-    else if (opt && !ourContext.parameters.opt1 && (!usrCmd || !opt)) {
+    else if (opt && !ourContext.parameters.opt1) {
         i1 = i + increment
         console.log("ques 2 triggered")
         console.log("context are : ", ourContext)
@@ -47,9 +43,7 @@ exports.gk = async (agent) => {
                 lifespan: 5,
                 "parameters": {
                     "opt1": opt, i1,
-                    score1,
-                    "Q1": data[0].Question[i],
-                    "Q": data[0].Question[i1]
+                    score1
                 }
             });
         }).catch(err => {
@@ -57,7 +51,7 @@ exports.gk = async (agent) => {
         })
     }
 
-    else if (ourContext.parameters.opt1 && !ourContext.parameters.opt2 && (!usrCmd || !opt)) {
+    else if (ourContext.parameters.opt1 && !ourContext.parameters.opt2) {
         i2 = ourContext.parameters.i1 + increment
         console.log("value of i is :", i)
         console.log("ques 3 triggered")
@@ -70,15 +64,13 @@ exports.gk = async (agent) => {
             score2 = ourContext.parameters.score1
         }
         await Model1.find({}).then(data => {
-            agent.add(`You said opt ${opt} . Here is your next question.${data[0].Question[i2]} `)
+            agent.add(`You said option ${opt} . Here is your next question.${data[0].Question[i2]} `)
             agent.setContext({
                 name: "abc",
                 lifespan: 5,
                 "parameters": {
                     "opt2": opt, i2,
-                    score2,
-                    "Q2": data[0].Question[i1],
-                    "Q": data[0].Question[i2]
+                    score2
                 }
             })
         }).catch(err => {
@@ -86,7 +78,7 @@ exports.gk = async (agent) => {
         })
     }
 
-    else if (ourContext.parameters.opt2 && !ourContext.parameters.opt3 && (!usrCmd || !opt)) {
+    else if (ourContext.parameters.opt2 && !ourContext.parameters.opt3) {
         i3 = ourContext.parameters.i2 + increment
         console.log("value of i is :", i)
         console.log("ques 4 triggered")
@@ -99,15 +91,13 @@ exports.gk = async (agent) => {
             score3 = ourContext.parameters.score2
         }
         await Model1.find({}).then(data => {
-            agent.add(`You said opt ${opt}. Here is your next question.${data[0].Question[i3]} `)
+            agent.add(`You said option ${opt}. Here is your next question.${data[0].Question[i3]} `)
             agent.setContext({
                 name: "abc",
                 lifespan: 5,
                 "parameters": {
                     "opt3": opt, i3,
-                    score3,
-                    "Q3": data[0].Question[i2],
-                    "Q": data[0].Question[i3]
+                    score3
                 }
             })
         }).catch(err => {
@@ -116,7 +106,7 @@ exports.gk = async (agent) => {
 
     }
 
-    else if (ourContext.parameters.opt3 && !ourContext.parameters.opt4 && (!usrCmd || !opt)) {
+    else if (ourContext.parameters.opt3 && !ourContext.parameters.opt4) {
         i4 = ourContext.parameters.i3 + increment
         console.log("ques 5 triggered")
         console.log("context are : ", ourContext)
@@ -128,15 +118,13 @@ exports.gk = async (agent) => {
             score4 = ourContext.parameters.score3
         }
         await Model1.find({}).then(data => {
-            agent.add(`You said opt ${opt}.Here is your next question.${data[0].Question[i4]} `)
+            agent.add(`You said option ${opt}.Here is your next question.${data[0].Question[i4]} `)
             agent.setContext({
                 name: "abc",
                 lifespan: 5,
                 "parameters": {
                     "opt4": opt, i4,
-                    score4,
-                    "Q4": data[0].Question[i3],
-                    "Q": data[0].Question[i4]
+                    score4
                 }
             });
         }).catch(err => {
@@ -144,7 +132,7 @@ exports.gk = async (agent) => {
         })
     }
 
-    else if (ourContext.parameters.opt4 && !ourContext.parameters.opt5 && (!usrCmd || !opt)) {
+    else if (ourContext.parameters.opt4 && !ourContext.parameters.opt5) {
         i5 = ourContext.parameters.i4 + increment
         console.log("ques 6 triggered")
         console.log("context are : ", ourContext)
@@ -156,15 +144,13 @@ exports.gk = async (agent) => {
             score5 = ourContext.parameters.score4
         }
         await Model1.find({}).then(data => {
-            agent.add(`You said opt ${opt}.Here is your next question.${data[0].Question[i5]}`)
+            agent.add(`You said option ${opt}.Here is your next question.${data[0].Question[i5]}`)
             agent.setContext({
                 name: "abc",
                 lifespan: 5,
                 "parameters": {
                     "opt5": opt, i5,
-                    score5,
-                    "Q5": data[0].Question[i4],
-                    "Q": data[0].Question[i5]
+                    score5
                 }
             });
         }).catch(err => {
@@ -172,7 +158,7 @@ exports.gk = async (agent) => {
         })
     }
 
-    else if (ourContext.parameters.opt5 && !ourContext.parameters.opt6 && (!usrCmd || !opt)) {
+    else if (ourContext.parameters.opt5 && !ourContext.parameters.opt6) {
         i6 = ourContext.parameters.i5 + increment
         console.log("ques 7 triggered")
         console.log("context are : ", ourContext)
@@ -184,15 +170,13 @@ exports.gk = async (agent) => {
             score6 = ourContext.parameters.score5
         }
         await Model1.find({}).then(data => {
-            agent.add(`You said opt ${opt}.Here is your next question.${data[0].Question[i6]}`)
+            agent.add(`You said option ${opt}.Here is your next question.${data[0].Question[i6]}`)
             agent.setContext({
                 name: "abc",
                 lifespan: 5,
                 "parameters": {
                     "opt6": opt, i6,
-                    score6,
-                    "Q6": data[0].Question[i5],
-                    "Q": data[0].Question[i6]
+                    score6
                 }
             });
         }).catch(err => {
@@ -200,7 +184,7 @@ exports.gk = async (agent) => {
         })
     }
 
-    else if (ourContext.parameters.opt6 && !ourContext.parameters.opt7 && (!usrCmd || !opt)) {
+    else if (ourContext.parameters.opt6 && !ourContext.parameters.opt7) {
         i7 = ourContext.parameters.i6 + increment
         console.log("ques 8 triggered")
         console.log("context are : ", ourContext)
@@ -212,15 +196,13 @@ exports.gk = async (agent) => {
             score7 = ourContext.parameters.score6
         }
         await Model1.find({}).then(data => {
-            agent.add(`You said opt ${opt}.Here is your next question.${data[0].Question[i7]}`)
+            agent.add(`You said option ${opt}.Here is your next question.${data[0].Question[i7]}`)
             agent.setContext({
                 name: "abc",
                 lifespan: 5,
                 "parameters": {
                     "opt7": opt, i7,
-                    score7,
-                    "Q7": data[0].Question[i6],
-                    "Q": data[0].Question[i7]
+                    score7
                 }
             });
         }).catch(err => {
@@ -228,7 +210,7 @@ exports.gk = async (agent) => {
         })
     }
 
-    else if (ourContext.parameters.opt7 && !ourContext.parameters.opt8 && (!usrCmd || !opt)) {
+    else if (ourContext.parameters.opt7 && !ourContext.parameters.opt8) {
         i8 = ourContext.parameters.i7 + increment
         console.log("ques 9 triggered")
         console.log("context are : ", ourContext)
@@ -246,9 +228,7 @@ exports.gk = async (agent) => {
                 lifespan: 5,
                 "parameters": {
                     "opt8": opt, i8,
-                    score8,
-                    "Q8": data[0].Question[i7],
-                    "Q": data[0].Question[i8]
+                    score8
                 }
             });
         }).catch(err => {
@@ -256,7 +236,7 @@ exports.gk = async (agent) => {
         })
     }
 
-    else if (ourContext.parameters.opt8 && !ourContext.parameters.opt9 && (!usrCmd || !opt)) {
+    else if (ourContext.parameters.opt8 && !ourContext.parameters.opt9) {
         i9 = ourContext.parameters.i8 + increment
         console.log("ques 10 triggered")
         console.log("context are : ", ourContext)
@@ -274,9 +254,7 @@ exports.gk = async (agent) => {
                 lifespan: 5,
                 "parameters": {
                     "opt9": opt, i9,
-                    score9,
-                    "Q9": data[0].Question[i8],
-                    "Q": data[0].Question[i9]
+                    score9
                 }
             });
         }).catch(err => {
@@ -296,7 +274,7 @@ exports.gk = async (agent) => {
         Model.find({}).then(data => {
             console.log("data is", data)
             if (data.filter((val) => val.Roll_No == ourContext.parameters.Roll_No).length) {
-                Model.findOneAndUpdate({ Roll_No: ourContext.parameters.Roll_No }, { Total_Score_in_GK: score10 }, (err, data) => {
+                Model.findOneAndUpdate({ Roll_No: ourContext.parameters.Roll_No }, { Total_Score_in_Science: score10, _v: 0 }, (err, data) => {
                     if (err) throw err
                     else {
                         console.log("Updated data is", data)
@@ -307,7 +285,7 @@ exports.gk = async (agent) => {
                 var info = {
                     Name: name,
                     Roll_No: ourContext.parameters.Roll_No,
-                    Total_Score_in_GK: score10,
+                    Total_Score_in_Science: score10
                 }
                 var saveData = new Model(info);
                 saveData.save((err, mydata) => {
@@ -315,27 +293,21 @@ exports.gk = async (agent) => {
                         console.log("error is:", err);
                     }
                     else {
-                        console.log("Save data is : ", mydata)
+                        console.log("data is : ", mydata)
                         return
                     }
                 });
             }
-            agent.add(`Congratulations you answered all 10 questions, ${score10} out of 10 was correct, 
-        Your score is ${(score10 * 100) / 10}%, Do you want me to send your transcript in your email?`)
-            agent.setContext({
-                name: "abc",
-                lifespan: 5,
-                "parameters": {
-                    "opt10": opt, i,
-                    score10,
-                
-                }
-            });
-        }).catch(err => {
-            console.log("error is : ", err)
         })
-    }
-    else if (usrCmd == 'read this question' || usrCmd == 'read this question again') {
-        agent.add(`Your question is ${ourContext.parameters.Q}`)
+        agent.add(`Congratulations you answered all 10 questions, ${score10} out of 10 was correct, 
+    Your score is ${(score10 * 100) / 10}%, Do you want me to send your transcript in your email?`)
+        agent.setContext({
+            name: "abc",
+            lifespan: 5,
+            "parameters": {
+                "opt10": opt, i,
+                score10
+            }
+        });
     }
 }
